5e6d71739c76c23a44883d49589bbe788ec14a27
Jonas Miederer <jm074@hdm-stuttgart.de>
2015-04-23 12:19:31 +0200

    externalLogin Google done Facebook done GitHub done Sing under construction

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -28,6 +28,45 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'facebook' => [
+                    'class' => 'yii\authclient\clients\Facebook',
+                    'clientId' => '751087841674690',
+                    'clientSecret' => 'eb48650a55ff164e5ff6bb123738c4b9',
+                ],
+                'google' => [
+                    'class' => 'yii\authclient\clients\GoogleOAuth',
+                    'clientId' => '58721707988-v5app0rim8mk4pqan11dq8hh95nvph2o.apps.googleusercontent.com',
+                    'clientSecret' => 'sNlR14tzduh9Z1n9SXyYodKZ'
+                ],
+                'github' => [
+                    'class' => 'yii\authclient\clients\GitHub',
+                    'clientId' => '3e128b840ff4613e0af4',
+                    'clientSecret' => '94c1823503f80cb2738ff19434d2c5eddcd95871'
+                ],
+                'twitter' => [
+                    'class' => 'yii\authclient\clients\Twitter',
+                    'consumerKey' => 'twitter_consumer_key',
+                    'consumerSecret' => 'twitter_consumer_secret',
+                ],
+                'linkedin' => [
+                    'class' => 'yii\authclient\clients\LinkedIn',
+                    'clientId' => 'linkedin_client_id',
+                    'clientSecret' => 'linkedin_client_secret',
+                ],
+                'xing' => [
+                    'class' => 'frontend\customAuth\Xing',
+                    'consumerKey' => '18cdca9317c098c0803e',
+                    'consumerSecret' => '9722ecc7f213cd8553c59b9131cd3f5cb2812dc7'
+                 ]
+            ],
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
     ],
     'params' => $params,
 ];
