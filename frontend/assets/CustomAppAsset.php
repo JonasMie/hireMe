82b29d5057b1f1df080fa4023001bfe808eee4ff
Marco Karaula <marco.karaula@googlemail.com>
2015-04-28 00:42:19 +0200

    Created a Custom App Asset and registered it in the main.php

---
+++ frontend/assets/CustomAppAsset.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Marco Karaula <marco.karaula@gmail.com>
+ * @since 2.0
+ */
+class CustomAppAsset extends AssetBundle
+{
+    public $css = [
+        'css/stylesheets/style.css',
+        'http://fonts.googleapis.com/css?family=PT+Sans'
+    ];
+
+}
