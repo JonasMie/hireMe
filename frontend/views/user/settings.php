d96f83936987f9bb1b2e50f76d1e8a791d95a347
Jonas Miederer <jm074@hdm-stuttgart.de>
2015-05-25 12:17:36 +0200

    initial settings commit

---
+++ frontend/views/user/settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by Jonas Miederer.
+ * User: jonas
+ * Date: 25.05.15
+ * Time: 12:04
+ * Project: hireMe
+ */
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+$this->title = 'Eintstellungen';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-settings">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+</div>
