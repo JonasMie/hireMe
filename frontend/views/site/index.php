35c6c8db0065655d6d37852173b5fb5d6468065f
petermorawski <pm041@hdm-stuttgart.de>
2015-05-30 02:26:46 +0200

    Startseite und Login-Formular: v0.0.0.0.1 :)

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,51 +1,31 @@
 <?php
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>hireMe</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p class="lead">Apply how, where and when you want.</p>
     </div>
+	
+	<div class="row">
+	
+	<div class="col-sm-4 col-sm-offset-1">
+        <h2>All your applications</h2>
 
-    <div class="body-content">
+        <p class="lead">On hireMe, once your data is entered, we securely store it in a standardized data format so it can be easily extracted and processed by the recruiter.</p>
+    </div>
+	
+	<div class="col-sm-4 col-sm-offset-2">
+        <h2>Just one click</h2>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+        <p class="lead">With hireMe, we introduced the hireMe-Button, which you'll soon find on all major job sites.</p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
+        <p><?= Html::a('Get started!', ['site/login',],['class' => 'btn btn-lg btn-success']) ?></p>
+    </div>
 
     </div>
 </div>
--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -2,6 +2,8 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\authclient\widgets\AuthChoice;
+use yii\bootstrap\Modal;
+use frontend\assets\SignupAsset;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
@@ -13,33 +15,79 @@ $this->registerJsFile("https://apis.google.com/js/platform.js", array('async'=>'
 $this->registerMetaTag(array('name' =>'google-signin-client_id', 'content'=>'58721707988-v5app0rim8mk4pqan11dq8hh95nvph2o.apps.googleusercontent.com'));
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
+
+// SignUp //
+include Yii::getAlias('@helper/companySignup.php');
+SignupAsset::register($this);
+
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
+	
+	<!-- Login Full-Page -->
 
     <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'email') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
+        <div class="col-sm-4 col-sm-offset-1 login-field">
+			<h1><?= Html::encode($this->title) ?></h1>
+		
+            <?php $form = ActiveForm::begin(['id' => 'login-form','layout' => 'horizontal']); ?>
+                <?= $form->field($model, 'email', ['template' => '{label} <div class="row"><div class="">{input}{error}{hint}</div></div>','inputOptions' => ['placeholder' => $model->getAttributeLabel('E-Mail')]])->label(false); ?>
+                <?= $form->field($model, 'password', ['template' => '{label} <div class="row"><div class="">{input}{error}{hint}</div></div>','inputOptions' => ['placeholder' => $model->getAttributeLabel('Passwort')]])->passwordInput()->label(false); ?>
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
+				
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-success login-button', 'name' => 'login-button']) ?>
+					<?= Html::a('Passwort vergessen?', ['site/request-password-reset']) ?>
                 </div>
             <?php ActiveForm::end(); ?>
+		
+			<?= yii\authclient\widgets\AuthChoice::widget([
+				'baseAuthUrl' => ['site/auth'],
+				'popupMode' => false
+			]) ?>
+			
         </div>
+		
+		<div class="col-sm-4 col-sm-offset-2 login-field">
+		
+			<h2>SignUp Formular</h2>
+		
+		</div>
+		
     </div>
+	
+	
+	<!-- Peter Login Modal Test -->
+	<?php
+	
+	Modal::begin([
+		'header' => '<h2>Hello world</h2>',
+		'toggleButton' => ['label' => 'click me'],
+	]);
 
-    <hr>
+	?>
+
+	<?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+		<?= $form->field($model, 'email') ?>
+		<?= $form->field($model, 'password')->passwordInput() ?>
+		<?= $form->field($model, 'rememberMe')->checkbox() ?>
+		<div style="color:#999;margin:1em 0">
+			If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+		</div>
+		<div class="form-group">
+			<?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+		</div>
+	<?php ActiveForm::end(); ?>
+	
+	<hr>
     <p><?= Yii::t('app', "Or login using another service:") ?></p>
 
     <?= yii\authclient\widgets\AuthChoice::widget([
         'baseAuthUrl' => ['site/auth'],
         'popupMode' => false,
     ]) ?>
+	
+	<?php
+	Modal::end();
+	
+	?>
 </div>
