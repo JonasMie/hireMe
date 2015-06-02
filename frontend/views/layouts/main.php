430eafd2a4529e40ed7df555e72c69f70be11c7d
Marco Karaula <marco.karaula@googlemail.com>
2015-06-02 16:54:27 +0200

    "Powered By" aus footer entfernt

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -86,8 +86,8 @@ CustomAppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-left">&copy; HireMe <?= date('Y') ?></p>
+        <!--<p class="pull-right"><?= Yii::powered() ?></p>-->
     </div>
 </footer>
 
