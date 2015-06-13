a0c45cce00c5b522b1b778675a4a8dfb4ed1da53
Simon Strobel <ss316@hdm-stuttgart.de>
2015-06-13 12:28:46 +0200

    Added auto increment to rest.

---
+++ console/migrations/m150613_101140_addAUTO_INCREMENTS.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150613_101140_addAUTO_INCREMENTS extends Migration
+{
+    public function up()
+    {
+        $this->alterColumn('application','id',SCHEMA::TYPE_INTEGER AUTO_INCREMENT);
+        $this->alterColumn('application_data','id',SCHEMA::TYPE_INTEGER AUTO_INCREMENT);
+        $this->alterColumn('favourites','id',SCHEMA::TYPE_INTEGER AUTO_INCREMENT);
+        $this->alterColumn('job','id',SCHEMA::TYPE_INTEGER AUTO_INCREMENT);
+
+    }
+
+    public function down()
+    {
+        echo "m150613_101140_addAUTO_INCREMENTS cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
