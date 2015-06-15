785cc2dee02053a9f3708d5fc67b06137f4c7aac
Marco Karaula <marco.karaula@googlemail.com>
2015-06-15 22:58:49 +0200

    Analytics Tiles

--- frontend/views/analytics/index.php
+++ frontend/views/analytics/index.php
@@ -14,22 +14,63 @@ $this->title = "Analytics für";
 
     <h1><?= Html::encode($this->title) ?> <?= $companyName ?></h1>
 
-    <p>
+
+
+    <div class="row" id="analytics-tiles">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 analytics-tile analytics-tile-1 tile-black ripple" onclick="window.location='./bewerbungen';">
+            <div class="subtile subtile-left">
+                <div class="tile-value tile-number">
+                    <?= Html::a($applyCount, "/bewerbungen"); ?>
+                </div>
+                <div class="tile-value tile-string">
+                    <?= Html::a('Bewerbungen insgesamt', "/bewerbungen"); ?>
+                </div>
+            </div>
+            <div class="subtile subtile-right hidden-xs">
+
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 analytics-tile analytics-tile-2 tile-green ripple">
+            <div class="subtile subtile-left">
+                <div class="tile-value tile-number">
+                    <div class="a-analytics"><?=$hiredCount?></div>
+                </div>
+                <div class="tile-value tile-string">
+                    <div class="a-analytics">Einstellungen</div>
+                </div>
+            </div>
+            <div class="subtile subtile-right hidden-xs">
+
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 analytics-tile analytics-tile-3 tile-black ripple" onclick="window.location='./job';">
+            <div class="subtile subtile-left">
+                <div class="tile-value tile-number">
+                    <?= Html::a($jobCount, "/job"); ?>
+                </div>
+                <div class="tile-value tile-string">
+                    <?= Html::a('Stellenanzeigen', "/job"); ?>
+                </div>
+            </div>
+            <div class="subtile subtile-right hidden-xs">
+
+            </div>
+        </div>
+
+
+    </div>
 
 
 
-
-    <h1><?= $applyCount ?> Bewerbungen</h1>
-    <h1><?= $hiredCount ?> Einstellungen</h1>
-    <h1><?= $jobCount ?> Stellenanzeigen</h1>
-    ---------------------------------------------------------------
     <h2><?= $clickCount  ?> Klicks bei <?= $viewCount ?> Views</h2>
     <h5>Interest Rate somit bei: <?= $interestRate ?> %</h5>
     <h2><?= $applyCount ?> Applications bei <?= $clickCount ?> Klicks</h2>
     <h5>Application Rate somit bei: <?= $applicationRate ?> %</h5>
     <h2>Interview Rate liegt bei: <?= $interviewRate ?> %</h2>
     <h2>Conversion Rate liegt bei: <?= $conversionRate ?> %</h2>
-    </p>
+
     <h1>-------------------------------------------------------------------------------</h1>
 
      <?=
