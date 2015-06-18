9589f5d69da86602168da3479d8850dbd7e47700
Simon Strobel <ss316@hdm-stuttgart.de>
2015-06-18 18:19:47 +0200

    Schöne karten wiederhergestellt. beim mergen aus versehen verloren gegangen

--- frontend/views/analytics/index.php
+++ frontend/views/analytics/index.php
@@ -10,19 +10,60 @@ CustomChartsAsset::register($this);
 $this->title = "Analytics für";
 
 ?>
+
+
 <div class="myjobs">
 
+   
     <h1><?= Html::encode($this->title) ?> <?= $companyName ?></h1>
 
-    
-    <p>
 
 
-    <h1><?= $applyCount ?> Bewerbungen</h1>
-    <h1><?= $hiredCount ?> Einstellungen</h1>
-    <h1><?= $jobCount ?> Stellenanzeigen</h1>
+    <div class="row" id="analytics-tiles">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 analytics-tile analytics-tile-1 tile-black ripple" onclick="window.location='./bewerbungen';">
+            <div class="subtile subtile-left">
+                <div class="tile-value tile-number">
+                    <?= Html::a($applyCount, "/bewerbungen"); ?>
+                </div>
+                <div class="tile-value tile-string">
+                    <?= Html::a('Bewerbungen insgesamt', "/bewerbungen"); ?>
+                </div>
+            </div>
+            <div class="subtile subtile-right hidden-xs">
 
-    <h1>-------------------------------------------------------------------------------</h1>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 analytics-tile analytics-tile-2 tile-green ripple">
+            <div class="subtile subtile-left">
+                <div class="tile-value tile-number">
+                    <div class="a-analytics"><?=$hiredCount?></div>
+                </div>
+                <div class="tile-value tile-string">
+                    <div class="a-analytics">Einstellungen</div>
+                </div>
+            </div>
+            <div class="subtile subtile-right hidden-xs">
+
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 analytics-tile analytics-tile-3 tile-black ripple" onclick="window.location='./job';">
+            <div class="subtile subtile-left">
+                <div class="tile-value tile-number">
+                    <?= Html::a($jobCount, "/job"); ?>
+                </div>
+                <div class="tile-value tile-string">
+                    <?= Html::a('Stellenanzeigen', "/job"); ?>
+                </div>
+            </div>
+            <div class="subtile subtile-right hidden-xs">
+
+            </div>
+        </div>
+
+    </div>
+
     <canvas id="viewClickChart" class="chart"></canvas>
     <canvas id="interestRateChart" class="chart"></canvas>
     <br>
