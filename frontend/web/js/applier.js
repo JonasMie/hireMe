ba49870eb3221c278e81bebea697ef0e3db278a1
petermorawski <pm041@hdm-stuttgart.de>
2015-06-22 19:58:35 +0200

    Button Popup; Login

--- frontend/web/js/applier.js
+++ frontend/web/js/applier.js
@@ -1,11 +1,11 @@
 
 $( document ).ready(function() {
-
+	
+	/* ApplyBtn Specific */
 	var key = $("#ac").attr('name');
-	var html = '<iframe src="http://frontend/job/view-count" width="0" height="0" id="hireMeFrame" scrolling="no" frameBorder="0" name="'+key+'">';
+	var html = '<iframe src="http://bewerbung.local/job/view-count" width="0" height="0" id="hireMeFrame" scrolling="no" frameBorder="0" name="'+key+'">';
 	$("#ac").html(html);
-	$("#ac").append("<button id='applyBtn' onclick='clicked();'>APPLY</button>");
-	$("#ac").append('<div style="width:400px; height:400px; border:1px solid;" id="hoverInfo"><iframe id="hoverInfoFrame" src="http://frontend/job/button-popup?key='+key+'" width="400" height="400" scrolling="no" frameBorder="0" name="'+key+'"></div>');
+	$("#ac").append("<button id='applyBtn'>APPLY</button>");
 	var btn = $( "#ac" ).children()[1];
 
 	console.log(btn);
@@ -15,20 +15,41 @@ $( document ).ready(function() {
    		 $('#hoverInfo').hide();
 		
 	});
+	
+	/* Center Popup Window on (Dual)Screen */
+	function PopupCenter(url, title, w, h) {
+		
+	}
+	
+	$(document).on('click', '#applyBtn', function(){ 
+    
+	// Fixes dual-screen position
+	var w = 650;
+	var h = 550;
+	var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+	var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+	width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+	height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+	var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+	var top = ((height / 2) - (h / 2)) + dualScreenTop;
+	var newWindow = window.open('http://bewerbung.local/job/button-popup?key='+key+'','_blank', 'toolbar=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+	// Puts focus on the newWindow
+	if (window.focus) {
+		newWindow.focus();
+	}
+	
+	
+	/*window.open("http://bewerbung.local/job/button-popup?key="+key+"", "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, width=400, height=400");*/
+    
+	
+	});
 
 });
 
-$(document).on('click', '#applyBtn', function(){ 
-     
-     $('#hoverInfo').show();
-     
- });
 
-$(document).on('mouseleave', '#hoverInfoFrame', function(){ 
-     
-     $('#hoverInfo').hide();
-     
- });
 
 
 
