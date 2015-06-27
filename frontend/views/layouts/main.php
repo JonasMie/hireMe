2f48768f5cc6739d14a57064c2f825cdf3b948d1
Marco Karaula <marco.karaula@googlemail.com>
2015-06-28 01:52:54 +0200

    Stupid Yii

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,8 +16,8 @@ CustomAppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-	<meta charset="<?= Yii::$app->charset ?>">
+	<link rel="shortcut icon" href="/css/favicon.ico" type="image/x-icon" />
+	 <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
