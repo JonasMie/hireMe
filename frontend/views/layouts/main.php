23656914dbbb68d988acd020cfffb3b8f30c1b5e
Marco Karaula <marco.karaula@googlemail.com>
2015-06-28 01:43:39 +0200

    Favicon erstellt

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,7 +16,8 @@ CustomAppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+	<meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
