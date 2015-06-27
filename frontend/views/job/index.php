5d83a9cc55465e16b3c1b2c06cb68f5b2681738f
Jonas Miederer <jm074@hdm-stuttgart.de>
2015-06-27 16:24:10 +0200

    favourites fix

--- frontend/views/job/index.php
+++ frontend/views/job/index.php
@@ -1,5 +1,6 @@
 ﻿<?php
 
+use frontend\models\Favourites;
 use yii\helpers\Html;
 use yii\widgets\ListView;
 use yii\grid\GridView;
@@ -34,13 +35,13 @@ $inFavourites = \frontend\models\Favourites::find()->where(['job_id' => $provide
 ?>
 
     <div class="myjobs">
-	
-	<? if (!(Yii::$app->user->identity->isRecruiter())) {
-			if(!($inFavourites)){
-				echo('<div class="col-sm-12 favouriteAlert"></div>');
-			}
-		}
-	?>
+
+        <? if (!(Yii::$app->user->identity->isRecruiter())) {
+            if (!($inFavourites)) {
+                echo('<div class="col-sm-12 favouriteAlert"></div>');
+            }
+        }
+        ?>
 
 
         <h1><?= Html::encode($this->title) ?></h1>
@@ -99,83 +100,83 @@ $inFavourites = \frontend\models\Favourites::find()->where(['job_id' => $provide
 
 
         <? else: ?>
-			<?= GridView::widget([
-				'dataProvider' => $provider,
-				'tableOptions' => ['class' => 'hireMeTable footable toggle-arrow', 'id' => 'jobListTable'],
-				'columns'      => [
-					[
-						'attribute'      => 'title',
-						'value'          => function ($data) {
-							return Html::a($data['title'], ['/job/view', 'id' => $data["id"]]);
-						},
-						'format'         => 'raw',
-						'label'          => 'Bezeichnung',
-						'headerOptions'  => ['class' => 'first-col'],
-						'contentOptions' => ['class' => 'first-col'],
-					],
-					[
-						'attribute'      => 'job_begin',
-						'label'          => 'Verfügbar ab',
-						'format'         => 'date',
-						'value'          => 'job_begin',
-						'headerOptions'  => ['class' => 'second-col'],
-						'contentOptions' => ['class' => 'second-col'],
-					],
-					[
-						'attribute'      => 'distance',
-						'value'          => function ($data) {
-							return round($data["distance"]) . " km";
-						},
-						'label'          => 'Entfernung',
-						// only show column is user has set his personal plz (otherwise no distance computation possible)
-						'visible'        => isset(Yii::$app->user->identity->geo_id),
-						'headerOptions'  => ['class' => 'third-col', 'data-hide' => 'mediaXXsmall,phone'],
-						'contentOptions' => ['class' => 'third-col'],
-					],
-					[
-						'attribute'      => 'city',
-						'label'          => 'Stadt',
-						'headerOptions'  => ['class' => 'fourth-col', 'data-hide' => 'mediaXXsmall,phone'],
-						'contentOptions' => ['class' => 'fourth-col'],
-					],
-					[
-						'class'    => \yii\grid\ActionColumn::className(),
-						'buttons'  =>
-							[
-								'view'   => function ($url, $model, $key) {
-									return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', 'job/view?id=' . $model["id"], ['title' => Yii::t('app', 'Ansehen')]);
-								},
-							],
-						'template' => '{view}',
-						'headerOptions'  => ['class' => 'fifth-col', 'data-hide' => 'mediaXXsmall,phone'],
-						'contentOptions' => ['class' => 'fifth-col'],
-					],
-					
-					[
-						'class'    => \yii\grid\ActionColumn::className(),
-						'buttons'  =>
-							[
-								'update' => function ($url, $model, $key) {
-									if(\frontend\models\Favourites::find()->where(['job_id' => $model->id, 'user_id' => Yii::$app->user->getId()])->count() > 0){
-									return Html::a('<span class="glyphicon glyphicon-star" id="toggleFavourite"></span>', '#', ['title' => Yii::t('app', 'Zu Favoriten hinzufügen')]);}
-									else return '';
-								}
-							],
-						'template' => '{update}',
-						'headerOptions'  => ['class' => 'sixth-col', 'data-hide' => 'mediaXXsmall,phone'],
-						'contentOptions' => ['class' => 'sixth-col'],
-					],
-					
-					[
+            <?= GridView::widget([
+                'dataProvider' => $provider,
+                'tableOptions' => ['class' => 'hireMeTable footable toggle-arrow', 'id' => 'jobListTable'],
+                'columns'      => [
+                    [
+                        'attribute'      => 'title',
+                        'value'          => function ($data) {
+                            return Html::a($data['title'], ['/job/view', 'id' => $data["id"]]);
+                        },
+                        'format'         => 'raw',
+                        'label'          => 'Bezeichnung',
+                        'headerOptions'  => ['class' => 'first-col'],
+                        'contentOptions' => ['class' => 'first-col'],
+                    ],
+                    [
+                        'attribute'      => 'job_begin',
+                        'label'          => 'Verfügbar ab',
+                        'format'         => 'date',
+                        'value'          => 'job_begin',
+                        'headerOptions'  => ['class' => 'second-col'],
+                        'contentOptions' => ['class' => 'second-col'],
+                    ],
+                    [
+                        'attribute'      => 'distance',
+                        'value'          => function ($data) {
+                            return round($data["distance"]) . " km";
+                        },
+                        'label'          => 'Entfernung',
+                        // only show column is user has set his personal plz (otherwise no distance computation possible)
+                        'visible'        => isset(Yii::$app->user->identity->geo_id),
+                        'headerOptions'  => ['class' => 'third-col', 'data-hide' => 'mediaXXsmall,phone'],
+                        'contentOptions' => ['class' => 'third-col'],
+                    ],
+                    [
+                        'attribute'      => 'city',
+                        'label'          => 'Stadt',
+                        'headerOptions'  => ['class' => 'fourth-col', 'data-hide' => 'mediaXXsmall,phone'],
+                        'contentOptions' => ['class' => 'fourth-col'],
+                    ],
+                    [
+                        'class'          => \yii\grid\ActionColumn::className(),
+                        'buttons'        =>
+                            [
+                                'view' => function ($url, $model, $key) {
+                                    return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', 'job/view?id=' . $model["id"], ['title' => Yii::t('app', 'Ansehen')]);
+                                },
+                            ],
+                        'template'       => '{view}',
+                        'headerOptions'  => ['class' => 'fifth-col', 'data-hide' => 'mediaXXsmall,phone'],
+                        'contentOptions' => ['class' => 'fifth-col'],
+                    ],
 
-						'class'          => 'yii\grid\Column',
-						'headerOptions'  => ['data-toggle' => 'true'],
-						'contentOptions' => ['data-title' => 'data-toggle', 'class' => 'sixth-col']
+                    [
+                        'class'          => \yii\grid\ActionColumn::className(),
+                        'buttons'        =>
+                            [
+                                'update' => function ($url, $model, $key) {
+                                    if (Favourites::find()->where(['job_id' => $model["id"], 'user_id' => Yii::$app->user->getId()])->count() == 0) {
+                                        return Html::a('<span class="glyphicon glyphicon-star" id="toggleFavourite"></span>', '#', ['title' => Yii::t('app', 'Zu Favoriten hinzufügen')]);
+                                    } else return '';
+                                }
+                            ],
+                        'template'       => '{update}',
+                        'headerOptions'  => ['class' => 'sixth-col', 'data-hide' => 'mediaXXsmall,phone'],
+                        'contentOptions' => ['class' => 'sixth-col'],
+                    ],
 
-					],
-				],
-			]);
-			?>
+                    [
+
+                        'class'          => 'yii\grid\Column',
+                        'headerOptions'  => ['data-toggle' => 'true'],
+                        'contentOptions' => ['data-title' => 'data-toggle', 'class' => 'sixth-col']
+
+                    ],
+                ],
+            ]);
+            ?>
 
             <div class="dropdown" id="searchDistance">
                 <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true"
@@ -197,7 +198,7 @@ $inFavourites = \frontend\models\Favourites::find()->where(['job_id' => $provide
             </div>
         <? endif; ?>
     </div>
-	
+
 <? // TODO: message if error
 $this->registerJs("jQuery('#toggleFavourite').click(function (e) {e.preventDefault(); \$this = jQuery(this);jQuery.post('/favourites/toggle', {id: \$this.data('job')}, function (res) {if (res.success) {\$this.remove();jQuery('.favouriteAlert').innerHtml('
 <div class=\"alert alert-success alert-dismissible\" role=\"alert\">
