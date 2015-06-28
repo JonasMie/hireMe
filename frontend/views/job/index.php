1a5defb9d4cdc4a92b32fdbcf7508d92a7063300
petermorawski <pm041@hdm-stuttgart.de>
2015-06-28 21:50:00 +0200

    Job View Fix...

--- frontend/views/job/index.php
+++ frontend/views/job/index.php
@@ -31,6 +31,8 @@ $inFavourites = \frontend\models\Favourites::find()->where(['job_id' => $provide
 	
 <h1><?= Html::encode($this->title) ?></h1>
 
+<div class="favouriteAlert"><div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Der Job wurde deinen Favoriten hinzugefügt. <a href="#" class="alert-link">Hier kannst du deine Favoriten ansehen.</a></div></div>
+
 <?/* Recruiter View */?>
 
 <? if (Yii::$app->user->identity->isRecruiter()): ?>
@@ -39,6 +41,7 @@ $inFavourites = \frontend\models\Favourites::find()->where(['job_id' => $provide
 <div class="row">
 	<div class="col-sm-9"></div>
 	<div class="col-sm-3 jobCreate"><?= Html::a("Neue Stellenanzeige", '/job/create',['class' => 'btn btn-success ripple']) ?></div>
+
 	<?=
 	ListView::widget([
 		'dataProvider' => $provider,
@@ -165,20 +168,36 @@ $inFavourites = \frontend\models\Favourites::find()->where(['job_id' => $provide
 				'contentOptions' => ['class' => 'fourth-col'],
 			],
 			[
-				'class'    => \yii\grid\ActionColumn::className(),
-				'buttons'  =>
+				'class'          => \yii\grid\ActionColumn::className(),
+				'buttons'        =>
 					[
-						'view'   => function ($url, $model, $key) {
+						'view' => function ($url, $model, $key) {
 							return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', 'job/view?id=' . $model["id"], ['title' => Yii::t('app', 'Ansehen')]);
 						},
-						'update' => function ($url, $model, $key) {
-							return Html::a('<span class="glyphicon glyphicon-star"></span>', '#', ['title' => Yii::t('app', 'Zu Favoriten hinzufügen')]);
-						}
 					],
-				'template' => '{view}{update}',
+				'template'       => '{view}',
 				'headerOptions'  => ['class' => 'fifth-col'],
 				'contentOptions' => ['class' => 'fifth-col'],
 			],
+
+			[
+				'class'          => \yii\grid\ActionColumn::className(),
+				'buttons'        =>
+					[
+						'update' => function ($url, $model, $key) {
+							if (Favourites::find()->where(['job_id' => $model["id"], 'user_id' => Yii::$app->user->getId()])->count() == 0) {
+								return Html::a('<span class="glyphicon glyphicon-star"></span>', '#', ['title' => Yii::t('app', 'Zu Favoriten hinzufügen'),'data-job' => $model["id"], 'class' =>"toggleFavourite"]);
+							} 
+							elseif (Favourites::find()->where(['job_id' => $model["id"], 'user_id' => Yii::$app->user->getId()])->count() == 1){
+								return '<span title="Bereits in Favoriten gespeichert\ class="glyphicon glyphicon-ok"></span>';
+							}
+						}
+					],
+				'template'       => '{update}',
+				'headerOptions'  => ['class' => 'sixth-col'],
+				'contentOptions' => ['class' => 'sixth-col'],
+			],
+
 			[
 
 				'class'          => 'yii\grid\Column',
@@ -192,3 +211,31 @@ $inFavourites = \frontend\models\Favourites::find()->where(['job_id' => $provide
 <? endif; ?>
 <?/* Applicant View END */?>
 </div>
+
+
+<? // TODO: message if error
+$this->registerJs("
+jQuery('#jobListTable').on( 'click', '.toggleFavourite', 
+	function (e) {
+		e.preventDefault();
+		\$this = jQuery(this);
+		jQuery.post('/favourites/toggle', {
+			id: \$this.data('job')
+		},	function (res) {
+				if (res.success) {
+					\$this.replaceWith('<span title=\"Bereits in Favoriten gespeichert\" class=\"highlight glyphicon glyphicon-ok\"></span>');
+					jQuery('.favouriteAlert').css('display','block').delay(1500).fadeOut(2000, function() {
+							$(this).css('display','none'); 
+						}
+					);
+					window.scrollTo(0, 0);
+				} 
+				else {
+					\$this.remove();
+				}
+			}
+		);
+	}
+);
+");
+?>
