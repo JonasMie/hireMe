88235bbf2b0dce45e6b37e1ba4476f0e7bb94a17
Marco Karaula <marco.karaula@googlemail.com>
2015-06-28 21:30:00 +0200

    Reihenfolge geändert

--- frontend/views/job/create.php
+++ frontend/views/job/create.php
@@ -20,64 +20,68 @@ $this->title = 'Stellenanzeige erstellen';
 
 ?>
 <div class="job-create">
+    <div class="row">
 
-	<div class="col-sm-6">
+        <h1><?= Html::encode($this->title) ?></h1>
 
-		<h1><?= Html::encode($this->title) ?></h1>
+        <?php $form = ActiveForm::begin(['id' => 'form-createJob']); ?>
 
-			<?php $form = ActiveForm::begin(['id' => 'form-createJob']); ?>
-                <?= $form->field($model, 'title')->label('Titel')?>
-                
-                <?= $form->field($model, 'description')->textarea() ?>
-                <?
-                echo DatePicker::widget([
-                    'model'         => $model,
-                    'attribute'     => 'job_begin',
-                    'attribute2'    => 'job_end',
-                    'options'       => ['placeholder' => 'Von'],
-                    'options2'      => ['placeholder' => 'Bis'],
-                    'type'          => DatePicker::TYPE_RANGE,
-                    'form'          => $form,
-                    'language'      => 'de',
-                    'separator'     => 'bis',
-                    'pluginOptions' => [
-                        'format'         => 'dd.mm.yyyy',
-                        'autoclose'      => true,
-                        'todayHighlight' => true,
-                    ]
-                ]);
-                ?>
+        <div class="col-sm-6">
 
-                 <?= $form->field($model, 'sector')->widget(\kartik\select2\Select2::className(), [
-                    'data' => $sectors,
-                    'options' => ['prompt' => ''],
-                ])->label('Branche auswählen') ?>
 
-                <?= $form->field($model, 'checkLocationBased')->checkbox(array('id'=>'checkLocationBased'))->label('Ortsbasiert') ?>
+            <?= $form->field($model, 'title')->label('Titel') ?>
 
-                <div class="locationDiv" style="display: none">    <? //STYLE: display in css?>
+            <?= $form->field($model, 'description', ['inputOptions' => ['class' => 'form-control', 'placeholder' => 'Beschreibung...']], ['options' => ['class' => 'form-control']])->textarea(['rows' => 15])->label(false) ?>
+
+        </div>
+
+        <div class="col-sm-6 col-2nd">
+
+            <?
+            echo DatePicker::widget([
+                'model' => $model,
+                'attribute' => 'job_begin',
+                'attribute2' => 'job_end',
+                'options' => ['placeholder' => 'Von'],
+                'options2' => ['placeholder' => 'Bis'],
+                'type' => DatePicker::TYPE_RANGE,
+                'form' => $form,
+                'language' => 'de',
+                'separator' => 'bis',
+                'pluginOptions' => [
+                    'format' => 'dd.mm.yyyy',
+                    'autoclose' => true,
+                    'todayHighlight' => true,
+                ]
+            ]);
+            ?>
+
+            <?= $form->field($model, 'sector')->widget(\kartik\select2\Select2::className(), [
+                'data' => $sectors,
+                'options' => ['prompt' => ''],
+            ])->label('Branche auswählen') ?>
+
+            <?= $form->field($model, 'checkLocationBased')->checkbox(array('id' => 'checkLocationBased'))->label('Ortsbasiert') ?>
+
+            <div class="locationDiv" style="display: none">    <? //STYLE: display in css?>
                 <?= $form->field($model, 'zip')->label('Postleitzahl') ?>
                 <?= $form->field($model, 'city')->label('Stadt') ?>
-                </div>
+            </div>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Erstellen', ['class' => 'btn btn-success', 'name' => 'create-button']) ?>
-                </div>
+            <div class="form-group field-submit">
+                <?= Html::submitButton('Erstellen', ['class' => 'btn btn-success', 'name' => 'create-button']) ?>
+            </div>
             <?php ActiveForm::end(); ?>
 
- <div id="aboutdiv">
-
- <?php Pjax::begin() ?>
-
-        <?= Html::a(
-            'Button generieren',
-            Url::to(['job/generation?id='.$assumedJobId]),
-            ['data-pjax'=> '#aboutdiv']
-        ) ?>
-
-    <?php Pjax::end(); ?>
-
- </div>
-
-</div>
+            <!-- <div id="aboutdiv">
+                <?php Pjax::begin() ?>
+                    <?= Html::a(
+                        'Button generieren',
+                        Url::to(['job/generation?id=' . $assumedJobId]),
+                        ['data-pjax' => '#aboutdiv']
+                    ) ?>
+                <?php Pjax::end(); ?>
+             </div> -->
+        </div>
+    </div>
 </div>
