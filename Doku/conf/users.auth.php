16eef48aed981560c420fc4329d5d16d64dc2e7f
Simon Strobel <ss316@hdm-stuttgart.de>
2015-08-18 12:38:13 +0200

    New Init

---
+++ Doku/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+:$1$0Pfm4NGM$TKXPQUzcvv7Yc6Q/LkBFQ/:::admin,user
